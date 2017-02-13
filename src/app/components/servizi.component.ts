a6e64f8531765a73465e6a40341eb89c3bc28ee9
Daudr <michidarin@gmail.com>
2017-02-13 21:38:12 +0100

    Aggiunte immagini servizi

--- src/app/components/servizi.component.ts
+++ src/app/components/servizi.component.ts
@@ -17,27 +17,27 @@ export class ServiziComponent implements OnInit, AfterViewInit {
 		},
 		{
 			nome: 'Casette enogastronomiche',
-			foto: '../../assets/icons/background/back-img.png',
+			foto: '../../assets/icons/services/casette.jpg',
 			descrizione: 'A Magie D\'Inverno sono presenti numerose casette ceh sapranno accontentarvi in tutto.' ,
 		},
 		{
 			nome: 'Noleggio Pattini',
-			foto: '../../assets/icons/background/back-img.png',
+			foto: '../../assets/icons/services/pattini.jpg',
 			descrizione: 'A Magie D\'Inverno sono presenti pattini che vanno dal 23 al 49, per tutti.' ,
 		},
 		{
 			nome: 'Taverna on Ice',
-			foto: '../../assets/icons/background/back-img.png',
+			foto: '../../assets/icons/services/taverna.jpg',
 			descrizione: 'Magie d\'Inverno offre una taverna affittabile a tutti i visitatori, per informazioni rivolgersi a Mirco Zago.',
 		},
 		{
 			nome: 'Gonfiabili e area bimbi',
-			foto: '../../assets/icons/background/back-img.png',
+			foto: '../../assets/icons/services/gonfiabili.jpg',
 			descrizione: 'A Magie D\'Inverno è presente un capannone riscaldato con al suo interno dei gonfiabili, un area bimbi e un bar, dove grandi e piccini potranno stare insieme e giocare.' ,
 		},
 		{
 			nome: 'Corsi di avviamento al pattinaggio della domenica mattina',
-			foto: '../../assets/icons/background/back-img.png',
+			foto: '../../assets/icons/services/corsimattina.jpg',
 			descrizione: 'Magie D\'Inverno offre ai più piccoli la possibilità di imparare a pattinare la domenica mattina' ,
 		},
 		{
