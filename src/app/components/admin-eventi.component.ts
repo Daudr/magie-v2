5185c7bc1a000189dc9011ffa57e16dcba0fb576
Michele Da Rin <michidarin@gmail.com>
2017-02-21 17:29:30 +0100

    Implementati i servizi per eventi e staff

--- src/app/components/admin-eventi.component.ts
+++ src/app/components/admin-eventi.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Eventi } from '../eventi';
+import { EventiService } from '../services/eventi.service';
+import { Staff } from '../staff';
+import { StaffService } from '../services/staff.service';
 
 @Component({
 	selector: 'admin-eventi',
 	templateUrl: './admin-eventi.component.html'
 })
-export class AdminEventiComponent { }
+export class AdminEventiComponent {
+  events: Eventi[];
+  staff: Staff[];
+  
+  constructor (eventiService: EventiService, staffService: StaffService) { }
+
+  ngOnInit () {
+    this.eventiService
+    .getEventi()
+    .then((events: Eventi[]) => {
+      this.events = events.map((events) => {
+        return events;
+      });
+    });
+    
+    this.staffService
+    .getStaff()
+    .then((staff: Staff[]) => {
+      this.staff = staff.map((staff) => {
+        return staff;
+      });
+    });
+  }
+}
