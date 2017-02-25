e1d9c06198debe88f0fbbd0d7b874597a5f97d16
Michele Da Rin <michidarin@gmail.com>
2017-02-25 16:27:32 +0100

    Update ice-team.component.ts

--- src/app/components/ice-team.component.ts
+++ src/app/components/ice-team.component.ts
@@ -2,6 +2,6 @@ import {Component } from '@angular/core';
 
 @Component({
   selector: 'ice-team',
-  templateUrl: './ice-team.component.hmtl'
+  templateUrl: './ice-team.component.html'
 })
 export class IceTeamComponent { }
