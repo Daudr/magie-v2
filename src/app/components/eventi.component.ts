7e1870a4b54bb7930efcf6eda981ed3249a9b5cc
Daudr <michidarin@gmail.com>
2017-02-27 15:27:25 +0100

    Aggiornati dialog eventi

--- src/app/components/eventi.component.ts
+++ src/app/components/eventi.component.ts
@@ -92,7 +92,7 @@ export class EventiComponent implements OnInit {
 			</span>
 		</div>
 		<br>
-		<div class="galleria row" *ngIf="event.galleria">
+		<div class="floatl row" *ngIf="event.galleria">
 			<img src={{foto}} class="col l4 m6 s12 materialboxed iceteam" *ngFor="let foto of event.galleria">
 		</div>
 	`
