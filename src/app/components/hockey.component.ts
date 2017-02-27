4764c3d77e35e4ce78a86a6e223a12cbb91b891f
Daudr <michidarin@gmail.com>
2017-02-27 13:21:20 +0100

    Aggiornata pagina Ice Team

--- src/app/components/hockey.component.ts
+++ src/app/components/hockey.component.ts
@@ -1,10 +1,27 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { Staff } from '../staff';
+import { StaffService } from '../services/staff.service';
 
 @Component({
 	selector: 'hockey',
 	templateUrl: './hockey.component.html'
 })
-export class HockeyComponent {
+export class HockeyComponent implements OnInit, AfterViewInit {
+	staff: Staff[];
+
+	constructor (private staffService: StaffService) { }
+
+	ngOnInit () {
+		this.staffService
+			.getCorsiStaff()
+			.then((staff: Staff[]) => {
+				this.staff = staff.map(( staff => {
+					return staff;
+				}));
+			});
+	}
+
 	ngAfterViewInit() {
 		if(!($('.mat-tab-list').hasClass('light-blue lighten-2 tab'))) {
 			$('.mat-tab-list').addClass('light-blue lighten-2 tab');
