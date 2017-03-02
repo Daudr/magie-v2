369d1ded467e2982db130d8780667b63c29466bc
Daudr <michidarin@gmail.com>
2017-03-02 21:41:05 +0100

    Aggiunto Service Worker [prova]

---
+++ src/sw.js
@@ -0,0 +1,39 @@
+// Polyfill for Chrome caching
+importScripts('/assets/js/cache-polyfill.js');
+
+// Install the ServiceWorker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+
+    // Open a cache
+    caches.open('v1').then(function(cache) {
+
+      // Define what we want to cache
+      return cache.addAll([
+        '/',
+        'index.html',
+        '/assets',
+        'inline.budle.js',
+        'styles.bundles.js',
+        'vendor.bundle.js',
+        'main.bundle.js',
+        'favicon.ico',
+      ]);
+    })
+  );
+});
+
+// Use ServiceWorker (or not) to fetch data
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+
+    // Look for something in the cache that matches the request
+    caches.match(event.request).then(function(response) {
+
+      // If we find something, return it
+      // Otherwise, use the network instead
+      return response || fetch(event.request);
+    })
+  );
+});
