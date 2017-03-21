e3e8c1482430eca5b7fdc6e11c1ab1c2ba819238
Michele Da Rin <michidarin@gmail.com>
2017-03-21 10:35:53 +0100

    Update servizi.component.ts

--- src/app/components/servizi.component.ts
+++ src/app/components/servizi.component.ts
@@ -41,13 +41,13 @@ export class ServiziComponent implements OnInit, AfterViewInit {
 			id: 'gonfiabili'
 		},
 		{
-			nome: 'Corsi di avviamento al pattinaggio della domenica mattina',
+			nome: 'Corsi di avviamento',
 			foto: '../../assets/icons/services/corsimattina.jpg',
 			descrizione: 'Magie D\'Inverno offre ai più piccoli la possibilità di imparare a pattinare la domenica mattina' ,
 			id: 'corsi'
 		},
 		{
-			nome: 'Corsi di avviamento, pattinaggio artistico e hockey del pomeriggio',
+			nome: 'Ice Team Sanve',
 			foto: '../../assets/icons/background/back-img.jpg',
 			descrizione: 'Magie d\'Inverno offre ai ragazzi la possibilità di imparare uno sport, poco conosciuto ma non per questo bello, a prezzi completamente convenienti.',
 			id: 'iceteam'
