c7170cff73678062ad2084c00a38f250158e28fd
Michele Da Rin <michidarin@gmail.com>
2017-05-09 21:12:08 +0200

    Aggiunto sendgrid [prova]

--- server.js
+++ server.js
@@ -291,28 +291,30 @@ app.get('*', function(req, res, next){
  *    DELETE: deletes contact by id
  */
 
-// app.get("/api/email", function(res, res){
-// 	var helper = require('sendgrid').mail;
+app.get("/api/email", function(res, res){
+  var helper = require('sendgrid').mail;
+  var fromEmail = new helper.Email('test@example.com');
+  var toEmail = new helper.Email('test@example.com');
+  var subject = 'Sending with SendGrid is Fun';
+  var content = new helper.Content('text/plain', 'and easy to do anywhere, even with Node.js');
+  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
-// 	from_email = new helper.Email("michidarin@gmail.com");
-// 	to_email = new helper.Email("michidarin@gmail.com");
-// 	subject = "Sending with SendGrid is Fun";
-// 	content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
-// 	mail = new helper.Mail(from_email, subject, to_email, content);
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
 
-// 	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// 	var request = sg.emptyRequest({
-// 	method: 'POST',
-// 	path: '/v3/mail/send',
-// 	body: mail.toJSON()
-// 	});
-
-// 	sg.API(request, function(error, response) {
-// 	console.log(response.statusCode);
-// 	console.log(response.body);
-// 	console.log(response.headers);
-// 	});
-// });
+  sg.API(request, function (error, response) {
+    if (error) {
+      console.log('Error response received');
+    }
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+});
 
 
 var server = app.listen(process.env.PORT || 8080, function () {
