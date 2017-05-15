f4526852ff57f647c339cc783f7d6c7ad199457c
Michele Da Rin <michidarin@gmail.com>
2017-05-15 17:28:15 +0200

    1 week cache

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(distDir));
 
 // HTTP Caching
 app.use(function(req, res, next) {
-    res.setHeader("Cache-Control", "max-age=604800");
+    res.setHeader("Cache-Control", "max-age=614800");
     return next();
 });
 
