8537a33a604c09529769136dc3d7ecc48fb071e8
Michele Da Rin <michidarin@gmail.com>
2017-06-04 11:38:48 +0200

    Cambiato tema Quill

--- src/app/components/admin-news.component.ts
+++ src/app/components/admin-news.component.ts
@@ -10,7 +10,14 @@ export class AdminNewsComponent implements AfterViewInit {
 
   ngAfterViewInit () {
     var editor = new Quill('#editor', {
-    modules: { toolbar: '#toolbar' },
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Scrivi qualcosa di bello...',
     theme: 'snow'
   });
   }
