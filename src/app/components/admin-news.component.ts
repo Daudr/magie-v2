7fd4f2b4fa237a649730ca296133b089246161a0
Michele Da Rin <michidarin@gmail.com>
2017-06-04 11:22:52 +0200

    Aggiunto Quill (prova)

--- src/app/components/admin-news.component.ts
+++ src/app/components/admin-news.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var Quill: any;
 
 @Component({
   selector: 'admin-news',
   templateUrl: './admin-news.component.html'
 })
-export class AdminNewsComponent {}
+export class AdminNewsComponent implements AfterViewInit {
+
+  ngAfterViewInit () {
+    var editor = new Quill('#editor', {
+    modules: { toolbar: '#toolbar' },
+    theme: 'bubble'
+  });
+  }
+}
