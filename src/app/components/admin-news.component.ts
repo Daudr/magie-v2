37b8507c28444a55fd193c9803c52c38b99c7ffb
Michele Da Rin <michidarin@gmail.com>
2017-06-04 21:34:59 +0200

    Mosso invio email nel posto giusto

--- src/app/components/admin-news.component.ts
+++ src/app/components/admin-news.component.ts
@@ -47,6 +47,6 @@ export class AdminNewsComponent implements AfterViewInit {
   sendMail () {
     console.log(this.editor.getText());
     console.log(this.editor.getContents());
-		this.news.sendMails("Questa è una prova", "Provare è bello");
+		// this.news.sendMails("Questa è una prova", "Provare è bello");
 	}
 }
