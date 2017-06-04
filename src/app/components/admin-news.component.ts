e8dc64206e47908ed060d278046a4443eb2b70db
Michele Da Rin <michidarin@gmail.com>
2017-06-04 21:19:24 +0200

    Rimosse alcune opzioni toolbar quill

--- src/app/components/admin-news.component.ts
+++ src/app/components/admin-news.component.ts
@@ -13,29 +13,25 @@ export class AdminNewsComponent implements AfterViewInit {
 
   openEditor () {
     var toolbarOptions = [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      ['blockquote', 'code-block'],
+      ['bold', 'italic', 'underline', 'strike'],
 
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
+      [{ 'indent': '-1'}, { 'indent': '+1' }],
 
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'color': [] }, { 'background': [] }],
       [{ 'font': [] }],
       [{ 'align': [] }],
 
-      ['clean']                                         // remove formatting button
+      ['clean']
     ];
     var options = {
       modules: {
         toolbar: toolbarOptions
       },
-      placeholder: 'Compose an epic...',
+      placeholder: 'Inserisci il contenuto dell\'email',
       theme: 'snow'
     };
     var editor = new Quill('#editor-quill', options);
