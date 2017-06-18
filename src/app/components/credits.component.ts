a325ca807bc7e5e49165fc8faa2f7aa5a6c782d7
Michele Da Rin <michidarin@gmail.com>
2017-06-18 14:55:53 +0200

    Aggiunte immagini sponsor /credits

--- src/app/components/credits.component.ts
+++ src/app/components/credits.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+import { Sponsor } from '../sponsor';
+
 @Component({
 	selector: 'credits',
 	templateUrl: './credits.component.html'
@@ -11,4 +13,31 @@ export class CreditsComponent implements OnInit {
 	ngOnInit () {
 		this.title.setTitle('Magie D\'Inverno - Crediti');
 	}
+
+	sponsors: Sponsor[] = [
+		{
+			icon: '../assets/icons/sponsor/ascopiave.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/banca.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/bibione.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/chocomax.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/csn.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/daros.jpg'
+		}/*,
+		{
+			icon: '../assets/icons/sponsor/epa.jpg'
+		},
+		{
+			icon: '../assets/icons/sponsor/gibus.jpg'
+		}*/
+	];
 }
