f5bf36c8acae90bb6432c6bfdcd789729914599a
Michele Da Rin <michidarin@gmail.com>
2017-07-30 17:21:10 +0200

    risolto errore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { MaterialModule } from '@angular/material';
 import {
   MdButtonModule,
   MdCardModule,
