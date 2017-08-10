415c2e37aae21fa2106923b3ea4fe5d9606f349e
Michele Da Rin <michidarin@gmail.com>
2017-08-10 22:42:54 +0200

    Aggiornato /sitemap

--- src/app/components/sitemap.component.ts
+++ src/app/components/sitemap.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'sitemap',
   templateUrl: './sitemap.component.html'
 })
-export class SitemapComponent {
+export class SitemapComponent implements OnInit {
+  constructor (private title: Title) {}
 
+  ngOnInit () {
+    this.title.setTitle('Magie D\'Inverno - Crediti');
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 }
