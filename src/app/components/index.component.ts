18d50cc7d5c6ab8036a3d24feb8af8cd0380d219
Michele Da Rin <michidarin@gmail.com>
2018-11-24 12:19:17 +0100

    Update index.component.ts

--- src/app/components/index.component.ts
+++ src/app/components/index.component.ts
@@ -104,7 +104,7 @@ export class IndexComponent implements OnInit, AfterViewInit {
 
 	isOpen(): string {
     const today = new Date();
-    if (today >= new Date(2018, 10, 17) && today <= new Date(2018, 2, 17)) {
+    if (today >= new Date(2018, 10, 17) && today <= new Date(2019, 2, 17)) {
      if (today >= new Date(2018, 11, 22) && today <= new Date(2019, 0, 6)) {
        return 'APERTI';
      } else {
